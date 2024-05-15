8e4a753fb2564a59cffdb9c722d31ee790f279d7
DELL <antrav07@gmail.com>
2024-05-15 22:29:43 +0530

    Add POJOs

---
+++ src/main/java/com/springframework/PetClinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springframework.PetClinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/springframework/PetClinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.springframework.PetClinic.model;
+
+public class Vet extends Person{
+}
