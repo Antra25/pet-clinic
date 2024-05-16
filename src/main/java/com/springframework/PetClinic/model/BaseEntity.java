7350520a73478914f3599ea421bbb7ccc860be35
DELL <antrav07@gmail.com>
2024-05-16 20:14:14 +0530

    Adding Base interface for services

--- src/main/java/com/springframework/PetClinic/model/BaseEntity.java
+++ src/main/java/com/springframework/PetClinic/model/BaseEntity.java
@@ -1,6 +1,8 @@
 package com.springframework.PetClinic.model;
 
-public class BaseEntity {
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
 
     private Long id;
 
